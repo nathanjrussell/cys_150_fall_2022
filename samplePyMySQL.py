eb78c9eae323e359e362db9f863c67bcdde2b9e4
Nathan J. Russell <6741335+nathanjrussell@users.noreply.github.com>
2022-11-07 16:24:07 -0500

    Add files via upload
    Simple pymysql code to demonstrate how to use pymysql library for connecting to mysql databases using python

---
+++ samplePyMySQL.py
@@ -0,0 +1,13 @@
+import pymysql
+
+conn = pymysql.connect(user="nathan", password="qwerty123456", db="nathan", host="bctc-rds.cu5dqz2itieu.us-east-1.rds.amazonaws.com", cursorclass=pymysql.cursors.DictCursor)
+
+with conn.cursor() as cursor:
+    sql = "select id_number, first_name, last_name, city, state from accounts"
+    cursor.execute(sql)
+    #conn.commit()
+    results = cursor.fetchall()
+    for result in results:
+        testStr = "Good Evening {0!s} {1!s}! Hope you had a great day in {2!s}, {3!s}".format(result['first_name'],result['last_name'],result['city'],result['state'])
+        print(testStr)
+    conn.close()
